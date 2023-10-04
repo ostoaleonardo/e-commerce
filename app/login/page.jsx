d6eabc07a8f1bb211b0cb05448357b2103ad1e90
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-10-04 14:06:58 -0600

    first commit

---
+++ app/login/page.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <main className='w-full min-h-screen flex flex-col items-center justify-center'>
+            <h1 className='text-5xl font-semibold'>Login</h1>
+        </main>
+    )
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className='w-full min-h-screen flex flex-col items-center justify-center'>
+            <h1 className='text-5xl font-semibold'>Home</h1>
+        </main>
+    )
+}
